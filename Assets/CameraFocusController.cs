8be425751edc3bd0393f33bfd843fe926fa5deaa
Herghys <helmi.santosa2@gmail.com>
2019-05-09 15:09:23 +0700

    Camera Fix
    Added Autofocus for Vuforia Cam

---
+++ Assets/CameraFocusController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class CameraFocusController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var vuforia = VuforiaARController.Instance;
+        vuforia.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+        vuforia.RegisterOnPauseCallback(OnPaused);
+    }
+
+    private void OnPaused(bool paused)
+    {
+        if (!paused)
+            CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    }
+
+    private void OnVuforiaStarted()
+    {
+        CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    }
+}
